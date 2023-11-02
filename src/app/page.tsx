bb7905ee777d38e05d0911fd07270bbde29319d8
Oli Evans <oli@protocol.ai>
2023-11-02 17:18:52 +0000

    feat: bones of the homepage 🦴
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+import { PropsWithChildren } from 'react'
+import { Web3StorageLogo } from '@/components/brand'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className='pb-36 relative grad-alt one-corn'>
+        <div className='grad'></div>
+        <header className='container mx-auto pt-10'>
+          <div className='flex items-center'>
+            <div className='flex-1'>
+              <Web3StorageLogo>web3.storage</Web3StorageLogo>
+            </div>
+            <nav>
+              <NavLink href='/features'>Features</NavLink>
+              <NavLink href='/pricing'>Pricing</NavLink>
+              <NavLink href='/faq'>FAQ</NavLink>
+              <NavLink href='/blog'>Blog</NavLink>
+              <NavLink href='/docs'>Docs</NavLink>
+              <ButtonLink href='https://console.web3.storage'>CONSOLE</ButtonLink>
+            </nav>
+          </div> 
+        </header>
+        <div className='max-w-7xl mx-auto pt-60 flex justify-items-stretch items-start'>
+          <div className='basis-1/2'>
+            <p className='text-7xl text-white font-medium'>Say hello to <br/>the data layer.</p>
+            <p className='pt-12'>
+              <ButtonLink href='/start'>Start now</ButtonLink>
+              <ButtonLink href='/faq'>What&apos;s the data layer?</ButtonLink>
+            </p>
+          </div>
+          <div className='basis-1/2 text-2xl text-white'>
+            <p>
+              Use decentralized protocols like IPFS and UCAN to liberate your data using our easy-to-use developer platform.</p>
+            <p className='pt-4'>We&apos;ll take care of the rest.</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section className='bg-zinc-950 text-white pt-20 pb-20 leading-loose one-corn'>
+        <div className='max-w-7xl mx-auto flex gap-8'>
+          <div className='w-1/2 pl-2 pr-26'>
+            <h2 className='font-bold text-2xl pb-8'>Connecting the web of data</h2>
+            <p>Eliminate silos with web3.storage. Using IPFS and other decentralized protocols, create a true data layer that connects you, your users, and the Web, regardless of where content is stored - client-side, in the cloud, or elsewhere.</p>
+            <p>Sounds hard? It isn’t. Our client libraries are super easy-to-use, abstracting the complexity of these decentralized protocols. And our hosted object storage provides best-in-class IPFS write and read performance and competitive pricing to web2 solutions, giving you the ability to write innovative applications without compromise.</p>
+          </div>
+        </div>
+      </section>
+      <section className='bg-blue-400 text-zinc-950 py-36 px-2 leading-loose grad-alt one-corn-top'>
+        <div className='bg-zinc-100/80 shadow-md rounded-md max-w-7xl mx-auto px-8 pt-8 pb-16'>
+          <h2 className='font-bold text-3xl pb-8'>web3.storage is built for scale</h2>
+          <div className='flex'>
+            <div className='w-1/2 text-center'>
+              <h3 className='text-6xl font-bold'>
+              <span className='grad-text'>200K+</span>
+              <span className='block text-lg'>USERS</span></h3>
+            </div>
+            <div className='w-1/2 text-center'>
+              <h3 className='text-6xl font-bold'>
+              <span className='grad-text'>200M+</span>
+              <span className='block text-lg'>STORED OBJECTS</span></h3>
+            </div>
+          </div>
+          <div className='lg:flex pt-28 gap-8'>
+            <div className='lg:w-1/2'>
+              <h2 className='font-bold text-3xl pb-2'>Why web3.storage?</h2>
+              <p>With web3.storage you get all the benefits of decentralized storage and other cutting-edge protocols with the frictionless experience you expect in a modern dev workflow. Check out our docs pages to learn more.</p>
+              <p className='pt-4'>Sounds hard? It isn’t. Our client libraries are super easy-to-use, abstracting the complexity of these decentralized protocols. And our hosted object storage provides best-in-class IPFS write and read performance and competitive pricing to web2 solutions, giving you the ability to write innovative applications without compromise.</p>
+              <p className='pt-4'><ButtonLink href='/docs'>Read docs</ButtonLink></p>
+            </div>
+            <div className='lg:w-1/2'>
+              <h3 className='pt-12 lg:pt-0'><span className='text-2xl font-bold grad-text'>Open</span></h3>
+              <p>All data is accessible via IPFS and backed by Filecoin storage, with service authentication using decentralized identity. No servers needed, no lock-in, no trust necessary.</p>
+              <h3 className='pt-4'><span className='text-2xl font-bold grad-text'>Reliable</span></h3>
+              <p>We take the best of web2 and web3 to provide infra you can rely on to scale with you. Frustration with decentralized storage is a thing of the past.</p>
+              <h3 className='pt-4'><span className='text-2xl font-bold grad-text'>Simple</span></h3>
+              <p>Start storing in minutes using w3up, our simple client library, to see how decentralized protocols can work together to unlock your data layer.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='bg-zinc-100 text-zinc-950 pt-20 pb-20 leading-loose'>
+        <div className='max-w-7xl mx-auto p-8'>
+          <div className='max-w-lg'>
+            <h2 className='font-bold text-4xl'>Get started with web3.storage</h2>
+            <p className='pt-2 text-xl font-light'>Choose your own way to store and retrieve using Web3.Storage. Use your email address or GitHub to get x GiB storage for free, with paid plans starting at $10.</p>
+            <p className='pt-4'><ButtonLink href='/start'>Create an account</ButtonLink></p>
+            <p className='pt-4'>Sounds hard? It isn’t. Our client libraries are super easy-to-use, abstracting the complexity of these decentralized protocols. And our hosted object storage provides best-in-class IPFS write and read performance and competitive pricing to web2 solutions, giving you the ability to write innovative applications without compromise.</p>
+          </div>
+        </div>
+      </section>
+      <footer className='bg-zinc-950'>
+        <p className='text-xl text-white text-center py-8'>⁂</p>
+      </footer>
     </main>
+  )
+}
+
+export function NavLink ({href, children}: {href:string, children: React.ReactNode}) {
+  return <Link href={href} className='uppercase font-medium text-xl p-2 mx-2'>{children}</Link>
+}
+
+export function ButtonLink ({href, children}: {href:string, children: React.ReactNode}) {
+  return (
+    <Link className='bg-zinc-950 text-white font-bold text-lg px-8 py-2 rounded-full ml-4 whitespace-nowrap' href={href}>
+      {children}
+    </Link>
   )
 }
