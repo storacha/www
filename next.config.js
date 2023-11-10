637468a075779ec8095d11ab8c6420f7a666088b
Oli Evans <oli@protocol.ai>
2023-11-10 10:54:32 +0000

    fix: output index.html files so export works in any http server and ipfs gateways
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  trailingSlash: true,
   reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ['shiki', 'vscode-oniguruma']
