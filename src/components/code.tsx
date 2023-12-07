0a2841627929dde36096eb611709234fde519a85
Alan Shaw <alan.shaw@protocol.ai>
2023-12-07 16:30:25 +0000

    feat: update example (#22)

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -2,23 +2,31 @@ import { highlight } from '@/lib/shiki'
 import { Lang, Theme } from 'shiki'
 
 const w3upExample = `
+import * as Client from '@web3-storage/w3up-client'
 import { filesFromPaths } from 'files-from-path'
-import { create } from '@web3-storage/w3up-client'
-const client = await create()
 
-// assert your identity
-await client.authorize('you@example.org')
+const client = await Client.create()
 
-// claim your space
-const space = await client.createSpace('lets-go')
-await client.setCurrentSpace(space.did())
+// first time setup!
+if (!Object.keys(client.accounts()).length) {
+  // waits for you to click the link in your email to verify your identity
+  const account = await client.login('you@example.org')
+
+  // create a space for your uploads
+  const space = await client.createSpace('lets-go')
+
+  // save the space to the store, and set as "current"
+  await space.save()
+
+  // associate this space with your account
+  await account.provision(space.did())
+}
 
 // content-address your files
 const files = await filesFromPaths(['./best-gifs'])
 const root = await client.uploadDirectory(files)
 
-// bafy...
-console.log(root.toString())
+console.log(root.toString()) // bafy...
 `.trim()
 
 export async function SyntaxHighlight ({code, theme = 'dracula-soft', lang = 'javascript'}: {code: string, theme?: Theme, lang?: Lang}) {
